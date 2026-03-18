abce9357a2e242f33c980a402b4a3d7b22ac89f4
Navyashree J S <navyashree0918@gmail.com>
2026-03-18 07:47:57 +0530

    Update SearchBox.jsx

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
     const [error, setError] = useState(false);
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "2869764014e5f1aeb15391b9035d5f94";
+    const API_KEY = your API_KEY;
 
     let getWeather = async () => {
         try {
